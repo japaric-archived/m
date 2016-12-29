e37cb7413fefcae68c43a4674be777e89c00a7b8
Jorge Aparicio <japaricious@gmail.com>
2016-12-28 22:06:39 -0500

    use cross to simplify CI

---
+++ ci/script.sh
@@ -0,0 +1,18 @@
+set -ex
+
+main() {
+    cross generate-lockfile
+
+    cross build --target $TARGET
+    cross build --target $TARGET --release
+
+    if [ $TARGET != s390x-unknown-linux-gnu ]; then
+        cross test --target $TARGET
+        cross test --target $TARGET --release
+
+        cross run --target $TARGET
+        cross run --target $TARGET --release
+    fi
+}
+
+main
